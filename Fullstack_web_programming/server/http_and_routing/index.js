973ac7a76fd4e455016356a79b96751a3676925f
Nico <nicolasperti@hotmail.com>
2024-10-06 10:29:28 +0200

    feat: express-Framework

--- Fullstack_web_programming/server/http_and_routing/index.js
+++ Fullstack_web_programming/server/http_and_routing/index.js
@@ -1,11 +1,63 @@
 const http = require('http');
-
-const port = 3000
+const cursos = require('./cursos');
+const port = 3000; 
 
 const server = http.createServer((req, res) => {
-    // Server logic
+    
 
-    res.end('...');
+    switch(req.method){
+        case 'GET':
+            if(req.url === '/cursos'){
+                res.statusCode = 200;
+                res.end(JSON.stringify(cursos));
+            } else {
+                res.statusCode = 404;
+                res.end('Not found');
+            }
+            break;
+        case 'POST':
+            if(req.url === '/cursos'){
+                res.statusCode = 201;
+                res.end('Curso creado');
+            } else {
+                res.statusCode = 404;
+                res.end('Not found');
+            }
+            break;
+        case 'PUT':
+            if(req.url === '/cursos'){
+                res.statusCode = 200;
+                res.end('Curso actualizado');
+            } else {
+                res.statusCode = 404;
+                res.end('Not found');
+            }
+            break;
+        case 'DELETE':
+            if(req.url === '/cursos'){
+                res.statusCode = 200;
+                res.end('Curso eliminado');
+            } else {
+                res.statusCode = 404;
+                res.end('Not found');
+            }
+            break;
+        default:
+            res.statusCode = 404;
+            res.end('Not found');
+            break;
+    }
+    // Server logic
+    console.log('Request url: ', req.url)
+    console.log('Request method: ', req.method)
+    console.log('Request headers: ', req.headers)
+    console.log('Request code: ', req.statusCode)
+    console.log('State code modified: ', res.statusCode)
+
+    res.setHeader('Content-Type', 'application/json');
+    console.log('Response headers: ', res.getHeaders())
+
+    res.end('Hello World');
 });
 
 server.listen(port, () => {
